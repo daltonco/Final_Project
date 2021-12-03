e67556831274c5aee86928fd987c51c30c38907f
daltonco <daltonco@mail.uc.edu>
2021-12-03 16:17:55 -0500

    Adding Colton 2/2

---
+++ Final_Project/Migrations/20211203201230_Mountains.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Final_Project.Migrations
+{
+    public partial class Mountains : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TallestMountains2",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    HeightInFeet = table.Column<int>(nullable: false),
+                    Country = table.Column<string>(nullable: true),
+                    Range = table.Column<string>(nullable: true),
+                    Parent = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TallestMountains2", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TallestMountains2",
+                columns: new[] { "Id", "Country", "HeightInFeet", "Name", "Parent", "Range" },
+                values: new object[,]
+                {
+                    { 1, "Nepal", 29029, "Mount Everest", "N/A", "Mahalangur Himalaya" },
+                    { 2, "Pakistan", 28251, "K2", "Mount Everest", "Baltoro Karakoram" },
+                    { 3, "Nepal", 28169, "Kangchenjunga", "Mount Everest", "Kangchenjunga Himalaya" },
+                    { 4, "Nepal", 27940, "Lhoste", "Mount Everest", "Mahalangur Himalaya" },
+                    { 5, "Nepal", 27838, "Makalu", "Mount Everest", "Mahalangur Himalaya" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TallestMountains2");
+        }
+    }
+}
