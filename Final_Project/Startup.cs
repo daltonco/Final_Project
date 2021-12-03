ae67c50a7d1b01a44679fff72bf30d7460b9d3d6
Samuel Untener <untenesj@mail.uc.edu>
2021-12-02 23:17:29 -0500

    done

--- Final_Project/Startup.cs
+++ Final_Project/Startup.cs
@@ -1,7 +1,10 @@
+using Final_Project.Data;
+using Final_Project.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,7 +13,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace Final_Project
 {
     public class Startup
@@ -19,18 +21,19 @@ namespace Final_Project
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<TallestTsunamisContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TallestTsunamisContext")));
             services.AddSwaggerDocument();
+            services.AddScoped<ITsunamiContextDAO, TsunamiContextDAO>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, TallestTsunamisContext context)
         {
 
             if (env.IsDevelopment())
@@ -38,20 +41,18 @@ namespace Final_Project
                 app.UseDeveloperExceptionPage();
             }
 
+            context.Database.Migrate();
+
             app.UseOpenApi();
             app.UseSwaggerUi3();
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-
         }
     }
 }
